fcc274f925d09e8df7e28df242a6c0105d2d23b0
Jonathan Cortes <124106713+Arcan2601@users.noreply.github.com>
2023-11-30 10:18:30 -0600

    clientes

--- src/app/clientes-update/clientes-update.component.ts
+++ src/app/clientes-update/clientes-update.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,Inject, OnInit } from '@angular/core';
+import { UpdateClientes } from '../models/cliente.model';
+import { MAT_DIALOG_DATA,MatDialogRef } from '@angular/material/dialog';
+import { ClientesService } from '../data.service';
 
 @Component({
   selector: 'app-clientes-update',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./clientes-update.component.css']
 })
 export class ClientesUpdateComponent {
+  clientes: UpdateClientes;
+  constructor(
+    public dialogRef: MatDialogRef<ClientesUpdateComponent>,
+    private clientesService: ClientesService,
+    @Inject(MAT_DIALOG_DATA) public data: UpdateClientes
 
+  ) {
+    // Clona los datos recibidos para evitar la mutación directa
+    this.clientes = {...data};
+  }
+
+  ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  guardar(): void {
+    this.clientesService.updateClientes(this.clientes).subscribe({
+      next: (response) => {
+        this.dialogRef.close(response);
+        location.reload();
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 }
