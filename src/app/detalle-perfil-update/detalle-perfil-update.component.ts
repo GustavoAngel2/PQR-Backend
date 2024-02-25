4a3084f8996511ddd8e0678a59fb82fcd35b29f2
Arkan2601 <Jonathancortesperez937@gmail.com>
2024-02-24 18:09:53 -0600

    detalleperfilList

--- src/app/detalle-perfil-update/detalle-perfil-update.component.ts
+++ src/app/detalle-perfil-update/detalle-perfil-update.component.ts
@@ -2,6 +2,9 @@ import { UpdateDetallePerfil } from '../models/detallePerfil.model';
 import { DetallePerfilService } from '../data.service';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA,MatDialogRef } from '@angular/material/dialog';
+import { RolesService } from '../data.service';
+import { ModulosService } from '../data.service';
+
 
 @Component({
   selector: 'app-detalle-perfil-update',
@@ -10,9 +13,16 @@ import { MAT_DIALOG_DATA,MatDialogRef } from '@angular/material/dialog';
 })
 export class DetallePerfilUpdateComponent implements OnInit{
   DetallePerfil: UpdateDetallePerfil;
+  ComboRol:any;
+  ComboModulo:any;
+  idPerfil!: number ;
+  idModulo!:number;
+
   constructor(
     public dialogRef: MatDialogRef<DetallePerfilUpdateComponent>,
     private DetallePerfilService: DetallePerfilService,
+    private rolesService: RolesService,
+    private modulosService: ModulosService,
     @Inject(MAT_DIALOG_DATA) public data: UpdateDetallePerfil
 
   ) {
@@ -21,6 +31,23 @@ export class DetallePerfilUpdateComponent implements OnInit{
   }
 
   ngOnInit(): void {
+    this.rolesService.getRoles().subscribe((data: any) => {
+    this.ComboRol = data;
+      console.log(this.ComboRol);
+    });
+    
+    this.modulosService.getModulos().subscribe((data2: any) => {
+    this.ComboModulo = data2;
+      console.log(this.ComboModulo);
+    });
+
+    this.idModulo = this.DetallePerfil.idModulo;
+    this.idPerfil = this.DetallePerfil.idPerfil;
+  }
+
+     dev(event: any): void {
+    this.idModulo = event.target.value;
+    this.idPerfil = event.target.value;
   }
 
   onNoClick(): void {
