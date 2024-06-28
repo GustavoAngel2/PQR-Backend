d08b808de5f964b92eb4189114449d206ca72365
Jonathan Cortes <124106713+Arkan2601@users.noreply.github.com>
2024-06-27 18:19:44 -0600

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+// app.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PQA2';
+export class AppComponent implements OnInit {
+  username: string = '';
+  idUsername: string = '';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.username = this.userService.getUsername();
+    this.idUsername = this.userService.getIdUsername();
+  }
 
   logout() {
     localStorage.removeItem('Token');
+    // Aquí podrías también limpiar los valores de username e idUsername en UserService si es necesario
+    this.userService.setUsername('');
+    this.userService.setIdUsername('');
   }
-
-
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { UserService } from './user.service'; // Importa el servicio de usuario aquí si es necesario
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,16 @@ import { catchError, map } from 'rxjs/operators';
 export class AuthService {
   private apiUrl = "http://localhost:5020/api";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private userService: UserService) { }
 
   login(credentials: { username: string, idUsername: string, userpassword: string }): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/SignIn`, credentials).pipe(
       map(response => {
         console.log('API response:', response);
-        if (response && response.Response && response.Response.data && response.Response.data.Token) {
-          localStorage.setItem('Token', response.Response.data.Token); 
+        if (response && response.Success && response.Response && response.Response.data && response.Response.data.Token) {
+          localStorage.setItem('Token', response.Response.data.Token);
+          this.userService.setIdUsername(response.Response.data.Usuario.Id.toString()); // Setear el Id en UserService (asegúrate de convertir a string si es necesario)
+          this.userService.setUsername(response.Response.data.Usuario.NombreUsuario); // Setear el NombreUsuario en UserService
           return response.Response.data;
         } else {
           throw new Error('Invalid API response');
@@ -28,9 +31,13 @@ export class AuthService {
       })
     );
   }
+  
+  
 
   logout() {
     localStorage.removeItem('Token');
+    this.userService.setIdUsername(''); // Limpiar el Id en UserService al cerrar sesión
+    this.userService.setUsername(''); // Limpiar el NombreUsuario en UserService al cerrar sesión
   }
 
   getToken() {
