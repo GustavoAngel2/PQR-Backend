4d09dbd503e0515ac336ce7e24e1d650748a51fb
GustavoAngel2 <124698903+GustavoAngel2@users.noreply.github.com>
2024-07-18 19:15:15 -0600

    -
    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -57,8 +57,6 @@ import { InicioComponent } from './inicio/inicio.component';
 import { ModulosComponent } from './modulos/modulos.component';
 import { ModulosUpdateComponent } from './modulos-update/modulos-update.component';
 import { EmpleadosComponent } from "./empleados/empleados.component";
-import { EmpleadosInsertComponent } from "./empleados-insert/empleados-insert.component";
-import { EmpleadosUpdateComponent } from "./empleados-update/empleados-update.component";
 import { PuestosComponent } from "./puestos/puestos.component";
 import { PuestosInsertComponent } from "./puestos-insert/puestos-insert.component";
 import { PuestosUpdateComponent } from "./puestos-update/puestos-update.component";
@@ -104,8 +102,6 @@ import { DialogsComponent } from './dialogs/dialogs.component';
     ModulosComponent,
     ModulosUpdateComponent,
     EmpleadosComponent,
-    EmpleadosInsertComponent,
-    EmpleadosUpdateComponent,
     PuestosComponent,
     PuestosInsertComponent,
     PuestosUpdateComponent,
